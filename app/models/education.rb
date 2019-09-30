e11daaf1bca112954a142bf075599df22af0d323
Raphael Souza <oliveira_raphael@msn.com>
2019-09-30 09:41:46 -0300

    Melhorando os Models e criando Validators

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,3 +1,7 @@
 class Education < ApplicationRecord
   belongs_to :portfolio
+  validates :description, presence: true
+  validates :start_date, presence: true
+  validates :institution, presence: true
+  validates_with DateIntervalValidator
 end
